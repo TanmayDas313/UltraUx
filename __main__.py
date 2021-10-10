5580f97bdfbcc2b14b3358e0af9ca9c6d948d095
Isane DV <92152410+dinu090907@users.noreply.github.com>
2021-10-10 18:14:17 +0530

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,5 @@
+PM_START_TEXT = """
+ʜᴇʟʟᴏ [🤗](https://telegra.ph/file/faefc45e3e7393df7fdcd.jpg), ɪᴍ ULTRA UX ᴀ ʜɪɢʜʏ ᴀᴅᴠᴀɴᴄᴇᴅ ʙᴏᴛ ᴡɪᴛʜ ʟᴏᴛꜱ ᴏꜰ ᴀᴍᴀᴢɪɴɢ ᴛᴏᴏʟꜱ. Made by @digvijaysingh178
+ʜᴇʟʟᴏ [🤗](https://telegra.ph/file/2e7e6412c7b2ba0d7eb25.jpg), ɪᴍ ULTRA UX ᴀ ʜɪɢʜʏ ᴀᴅᴠᴀɴᴄᴇᴅ ʙᴏᴛ ᴡɪᴛʜ ʟᴏᴛꜱ ᴏꜰ ᴀᴍᴀᴢɪɴɢ ᴛᴏᴏʟꜱ. Made by @digvijaysingh178
+`ɪ'ᴍ ʜᴇʀᴇ ᴛᴏ ʜᴇʟᴘ ʏᴏᴜ ᴍᴀɴᴀɢᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘꜱ! ʜɪᴛ` /help   
+"""
